09f2a12b19a6c361aa579275372ed89d0b0cb17a
Thibaut RETY <thibaut.rety@gmail.com>
2020-12-16 14:58:14 +0100

    andriod config

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -62,7 +62,7 @@ export class UserService {
    * the user entered on the form.
    */
   signup(accountInfo: any) {
-    return this.apiService.post('register', accountInfo, { responseType: 'text' as 'text' }).pipe(share());
+    return this.apiService.post('register', accountInfo, { responseType: 'json' as 'json' }).pipe(share());
   }
 
   /**
